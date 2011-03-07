471ef93aca74c477a84b91179ee426add877b39d
Matt J <exabytes18@gmail.com>
2011-03-07 10:49:59 -0600

    Cleaning up code

--- src/TitleTagRegex.java
+++ src/TitleTagRegex.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 
 public class TitleTagRegex {
 
-	private static String getHTMLFromStdIn() throws IOException {
+	private static String getInputFromStdIn() throws IOException {
 		StringBuilder sb = new StringBuilder();
 		InputStreamReader stream = new InputStreamReader(System.in);
 		BufferedReader in = new BufferedReader(stream);
@@ -17,32 +17,37 @@ public class TitleTagRegex {
 		return sb.toString();
 	}
 
-	public static void main(String[] args) {
+	/**
+	 * Entry point for the program. The program grabs raw html from stdin and
+	 * prints out the title contained within.
+	 * 
+	 * @param args
+	 *            None
+	 * @throws IOException
+	 *             Pipe failure.
+	 */
+	public static void main(String[] args) throws IOException {
 		/*
 		 * Note: java seems to correctly choose the most specific rule for the
-		 * occasion. Notice that the first 3 rules all begin with the same
-		 * characters, yet it chooses the most specific as soon as it can
+		 * occasion. Notice that the first 4 rules all begin with the same
+		 * character, yet it chooses the most specific as soon as it can
 		 * logically deduce which rule applies.
 		 */
-		String cdata = "(?:\\<!\\[CDATA\\[.*?\\]\\]>)"; // match cdata
+		String cdre = "(?:\\<!\\[CDATA\\[.*?\\]\\]>)"; // match cdata
 		String cre = "(?:\\<!--.*?--[\\s]*>)"; // match any comment
-		String mdcl = "(?:\\<!.*?>)"; // match any remaining markup declaration
+		String mdre = "(?:\\<!.*?>)"; // match any remaining markup declaration
 		String tre = "(?:\\<[^!].*?>)"; // match any tag
-		String slit = "(?:[^<]*)"; // match any string literal
+		String slre = "(?:[^<]*)"; // match any string literal
 
-		String nonComment = "(?:" + cdata + "|" + cre + "|" + mdcl + "|" + tre + "|" + slit + ")";
-		String html = "(?:" + nonComment + ")*?<title>(.*?)</title>(?:" + nonComment + ")*";
+		String nonComment = "(?:" + cdre + "|" + cre + "|" + mdre + "|" + tre + "|" + slre + ")";
+		String html = nonComment + "*?<title>(.*?)</title>" + nonComment + "*";
 		String entirety = "\\A" + html + "\\z";
 
 		int flags = Pattern.DOTALL | Pattern.CASE_INSENSITIVE;
 		Pattern p = Pattern.compile(entirety, flags);
-		try {
-			Matcher m = p.matcher(getHTMLFromStdIn());
-			if (m.find()) {
-				System.out.println(m.group(1));
-			}
-		} catch (IOException e) {
-			System.err.println("usage: java TitleTagRegex < html");
+		Matcher m = p.matcher(getInputFromStdIn());
+		if (m.find()) {
+			System.out.println(m.group(1));
 		}
 	}
 
